8e0d7838c62817766ff41f9490b4c14dbe56d8f0
William  Johnston <william.johnston@Cbus-Air-197.local>
2020-12-03 15:48:14 -0500

    Added content to FigmaDetail.js; used ReactDetail as template.

--- src/components/Home/Tools/FigmaDetail.js
+++ src/components/Home/Tools/FigmaDetail.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useContext }from 'react'
+import styled from 'styled-components'
+import { useTransition, animated } from 'react-spring'
 
+import { ViewContext } from '../../../Store/ViewContext'
+
+import useIsDev from '../../../Utilities/Hooks/useDevStyle'
+import SkillsHeader from '../../Layout/Elements/SkillsHeader'
+import SkillBar from '../../Layout/Elements/SkillBar'
+import SkillMarquee from '../../Layout/Elements/SkillMarquee'
+
+const ComponentWrapper = styled(animated.section)`
+    margin: 1rem;
+    display: flex;
+    flex-direction: column;
+`
 export default function FigmaDetail() {
+
+    const { viewData } = useContext(ViewContext)
+    const compStyle = useIsDev(viewData.isDev,"dotted thin", "orange");
+
     return (
-        <div>
-           Figma Detail 
-        </div>
-    )
-}
+        <ComponentWrapper 
+            style={compStyle}
+        >
+            <SkillMarquee text={'Figma'} />
+            <SkillsHeader
+                firstLabel={'Skill'}
+                secondLabel={'Level'}
+            />
+            <SkillBar
+                skillName={'Wireframes'}
+                skillLevel={[true,true,true,true,true]}
+            />
+            <SkillBar
+                skillName={'Feature Prototyping'}
+                skillLevel={[true,true,true,true,true]}
+            />
+            <SkillBar
+                skillName={'Components / Design Systems'}
+                skillLevel={[true,true,true,true,false]}
+            />
+            <SkillBar
+                skillName={'Interactions'}
+                skillLevel={[true,true,true,true,true]}
+            />
+            <SkillBar
+                skillName={'Atomic Design'}
+                skillLevel={[true,true,true,false,false]}
+            />
+        </ComponentWrapper>
+     )
+    }
+        
